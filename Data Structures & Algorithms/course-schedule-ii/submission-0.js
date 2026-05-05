fdce2246d4d95f924699fc93e60d52ca0bf5594f
khwaja-k-mohammad <khwaja.k.mohammad@gmail.com>
2026-05-05 23:36:29 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {number[]}
+     */
+    findOrder(numCourses, prerequisites) {
+
+        let coursesToClear = new Array(numCourses).fill(0);
+        let dependantCoursesList = Array.from({ length: numCourses }, () => []);
+
+        for(let [a,b] of prerequisites)
+        {
+            coursesToClear[a] += 1;
+            dependantCoursesList[b].push(a);
+        }
+
+        let queue = [];
+        for(let i=0;i<numCourses;i++)
+        {
+            if(coursesToClear[i] == 0) queue.push(i);
+        }
+
+        const order = [];
+        let completed = 0;
+        while(queue.length > 0)
+        {
+            const c = queue.shift();
+            order.push(c);
+            completed += 1;
+            for(let neighbour of dependantCoursesList[c])
+            {
+                coursesToClear[neighbour]--;
+                if(coursesToClear[neighbour] == 0){
+                    queue.push(neighbour);
+                }
+            }
+        }
+
+        return completed == numCourses ? order : [];
+    }
+}
