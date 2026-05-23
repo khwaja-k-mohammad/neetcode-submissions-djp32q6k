970cc0757a1343d34c9c640c278141a0aa62c30e
khwaja-k-mohammad <khwaja.k.mohammad@gmail.com>
2026-05-23 23:27:48 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int xorVal = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            xorVal ^= nums[i];
+        }
+
+        return xorVal;
+    }
+};
